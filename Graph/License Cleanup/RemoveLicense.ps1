69325c6a0b6d02f0e36350b223922f87245470ac
Raheem <159470974+ChismanRaheem@users.noreply.github.com>
2024-06-24 10:59:44 -0700

    Update RemoveLicense.ps1

--- Graph/License Cleanup/RemoveLicense.ps1
+++ Graph/License Cleanup/RemoveLicense.ps1
@@ -14,7 +14,7 @@
 #
 
 
-Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All -TenantId 85086bfd-eeee-40c4-b751-ea94d9159ea3
+Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All -TenantId 85086bfd-eeee-random-b751-randomPii
 $e3PBI = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'POWER_BI_STANDARD'
 $EmsSku = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'EMSPREMIUM'
 
