f677cca3b1cefbf7731906aa9d04e743ff80addc
Raheem <159470974+ChismanRaheem@users.noreply.github.com>
2024-07-16 11:27:24 -0700

    Update AssignEntrarolesPIM.ps1

--- Graph/Identity-Governance/AssignEntrarolesPIM.ps1
+++ Graph/Identity-Governance/AssignEntrarolesPIM.ps1
@@ -1,1 +1,37 @@
+#To use the Microsoft Entra Privileged Identity Management, you must have one of the following licenses:
+#Microsoft Entra ID P2
+#Enterprise Mobility + Security (EMS) E5 license
+#Microsoft Graph PowerShell using a Privileged Role Administrator role and the appropriate permissions. For this tutorial, the RoleManagement.ReadWrite.Directory delegated permission is required. To set the permissions in Microsoft Graph PowerShell, run;
+# Ref: https://learn.microsoft.com/en-us/powershell/microsoftgraph/tutorial-pim?view=graph-powershell-1.0
 
+#Set UPN of test  user
+$UPN = "Raheem@FQDN.onmicrosoft.com"
+
+# Connect to graph with required scope using DeviveCode flow.
+Connect-MgGraph -TenantId 536279f6-15cc-45f2-be2d-61e352b51eef -ContextScope Process -UseDeviceAuthentication -Scopes "RoleManagement.ReadWrite.Directory"
+
+#Set user based on UPN to capture id
+$User = Get-MgUser -Search "UserPrincipalName:$($UPN)" -ConsistencyLevel eventual -Count userCount 
+
+#Capture Roles EligibilitySchedule
+$Roles = Get-MgRoleManagementDirectoryRoleEligibilityScheduleInstance -Filter "principalId eq '$($User.Id)'" 
+
+# Set  the parameter for selfactivation for pim 
+$params = @{
+  "PrincipalId" = $User.Id
+  "RoleDefinitionId" = "88d8e3e3-8f55-4a1e-953a-9b9898b8876b"
+  "Justification" = "Activate assignment"
+  "DirectoryScopeId" = "/"
+  "Action" = "SelfActivate"
+  "ScheduleInfo" = @{
+    "StartDateTime" = Get-Date
+    "Expiration" = @{
+       "Type" = "AfterDuration"
+       "Duration" = "PT1H"
+       }
+     }
+    }
+
+#commnad execution
+    New-MgRoleManagementDirectoryRoleAssignmentScheduleRequest -BodyParameter $params |
+  Format-List Id, Status, Action, AppScopeId, DirectoryScopeId, RoleDefinitionID, IsValidationOnly, Justification, PrincipalId, CompletedDateTime, CreatedDateTime, TargetScheduleID
